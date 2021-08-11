3ae5b9e733ea471c2de36190c96992b204b37b71
sue445 <sue445@sue445.net>
2021-08-11 21:35:58 +0900

    Add bench

--- bench.rb
+++ bench.rb
@@ -18,7 +18,27 @@ Benchmark.ips do |x|
     marshal_dump_and_load(obj)
   end
 
+  x.report("Marshal.dump") do
+    str = Marshal.dump(obj)
+  end
+
+  str = Marshal.dump(obj)
+
+  x.report("Marshal.load") do
+    Marshal.load(str)
+  end
+
   x.report("to_json and JSON.load") do
     to_json_and_load_json(obj)
+  end
+
+  x.report("to_json") do
+    json = obj.to_json
+  end
+
+  json = obj.to_json
+
+  x.report("JSON.load") do
+    JSON.load(json)
   end
 end
